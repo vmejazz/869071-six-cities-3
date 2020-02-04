eb7f17687988bb618ea8b3f85dedc3ef48ab646d
vmejazz <vmejazz@gmail.com>
2020-02-04 20:53:08 +0300

    Special fix for Travis

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 
-const OFFER_PLACES_COUNT = 312;
+const OFFER_PLACES_COUNT = 313;
 
 ReactDOM.render(
     <App
