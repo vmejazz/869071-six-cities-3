ea3444dbec5637bb2901a8a8a9881dc83822ec84
vmejazz <vmejazz@gmail.com>
2020-02-11 14:18:57 +0300

    Conversion func-component to class-component

--- src/components/apartment-list/apartment-list.jsx
+++ src/components/apartment-list/apartment-list.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import ApartmentCard from "../apartment-card/apartment-card.jsx";
 
-const ApartmentList = (props) => {
-  const {offersArray, onCityTitleClick} = props;
+class ApartmentList extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="cities__places-list places__list tabs__content">
-      {offersArray.map((item) => {
-        return (
-          <ApartmentCard
-            key={item.id}
-            placeOffer={item}
-            onCityTitleClick={onCityTitleClick}
-          />
-        );
-      })}
-    </div>
-  );
-};
+  }
+
+  render() {
+    const {offersArray, onCityTitleClick} = this.props;
+
+    return (
+      <div className="cities__places-list places__list tabs__content">
+        {offersArray.map((item) => {
+          return (
+            <ApartmentCard
+              key={item.id}
+              placeOffer={item}
+              onCityTitleClick={onCityTitleClick}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 ApartmentList.propTypes = {
   offersArray: PropTypes.arrayOf(PropTypes.object).isRequired,
