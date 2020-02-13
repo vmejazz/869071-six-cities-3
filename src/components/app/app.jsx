30447fb81dc85bed00b007d59cc469be8d6812ef
vmejazz <vmejazz@gmail.com>
2020-02-13 13:58:45 +0300

    Conversation App func-comp to class-comp

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Main from "../main/main.jsx";
 import ApartmentDetailInfo from "../apartment-detail-info/apartment-detail-info.jsx";
 
-const App = (props) => {
-  const {offerPlacesCount, offersArray, onCityTitleClick} = props;
-  const offer = offersArray[0];
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Main
-            offerPlacesCount={offerPlacesCount}
-            offersArray={offersArray}
-            onCityTitleClick={onCityTitleClick}/>
-        </Route>
-        <Route exact path="/dev-apartment-detail-info">
-          <ApartmentDetailInfo offer={offer}/>
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  );
-};
+    this.state = {
+      link: `/`
+    };
+  }
+
+  render() {
+    const {offerPlacesCount, offersArray, onCityTitleClick} = this.props;
+    const offer = offersArray[0];
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main
+              offerPlacesCount={offerPlacesCount}
+              offersArray={offersArray}
+              onCityTitleClick={onCityTitleClick}/>
+          </Route>
+          <Route exact path="/dev-apartment-detail-info">
+            <ApartmentDetailInfo offer={offer}/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   offerPlacesCount: PropTypes.number.isRequired,
