8d3154107d665b89429470eab9dfbb5f957cbffc
vmejazz <vmejazz@gmail.com>
2020-02-21 19:45:42 +0300

    Fix e2e test for apartment-card

--- src/components/apartment-card/apartment-card.e2e.test.js
+++ src/components/apartment-card/apartment-card.e2e.test.js
@@ -9,36 +9,38 @@ Enzyme.configure({
 });
 
 const placeOffer = {
-  id: 1,
+  id: 5,
   title: `Best Apartment`
 };
 
 const offersArray = [
   {
-    id: 1,
+    id: 3,
     title: `First apartment`
   }
 ];
 
 describe(``, () => {
-  it(`Should button be pressed`, () => {
-    const onMouseHover = jest.fn();
+  it(`Should button be pressed and retern ID card`, () => {
+    const onApartmentCardClick = jest.fn();
 
     const tree = shallow(
         <ApartmentCard
           placeOffer={placeOffer}
-          onMouseHover={onMouseHover}
+          onApartmentCardClick={onApartmentCardClick}
+          onMouseHover={() => {}}
         />
     );
 
     const apartmentArticle = tree.find(`.place-card`);
 
-    apartmentArticle.props().onMouseEnter();
+    apartmentArticle.props().onClick();
 
-    expect(onMouseHover.mock.calls.length).toBe(1);
+    // expect(onApartmentCardClick.mock.calls.length).toBe(1);
+    expect(onApartmentCardClick).toHaveBeenCalledWith(5);
   });
 
-  it(`Should change ActideCardID`, () => {
+  it(`Should change ActideCardID when mouse hover on card`, () => {
     const onMouseHover = jest.fn();
 
     const tree = mount(
@@ -52,7 +54,9 @@ describe(``, () => {
 
     expect(tree.state().activeCardId).toEqual(-1);
     apartmentArticle.props().onMouseEnter();
-    expect(tree.state().activeCardId).toEqual(1);
+    expect(tree.state().activeCardId).toEqual(3);
+    apartmentArticle.props().onMouseLeave();
+    expect(tree.state().activeCardId).toEqual(-1);
   });
 });
 
