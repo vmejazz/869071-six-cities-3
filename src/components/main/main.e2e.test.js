d64014f5bc756b6e63055927d8a046fc60020d4a
vmejazz <vmejazz@gmail.com>
2020-02-21 19:18:23 +0300

    Fix warning for MAP component in enzyme test

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Main from "./main.jsx";
-jest.mock(`../map/map.jsx`, () => `Map`);
+jest.mock(`../map/map.jsx`);
 
 Enzyme.configure({
   adapter: new Adapter()
