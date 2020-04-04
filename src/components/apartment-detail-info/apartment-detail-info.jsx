171f86b4968ae0315ad7ce5a010d746dc2033c69
vmejazz <vmejazz@gmail.com>
2020-04-04 11:57:59 +0300

    Convert class-component to func-component

--- src/components/apartment-detail-info/apartment-detail-info.jsx
+++ src/components/apartment-detail-info/apartment-detail-info.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import ReviewsList from "../reviews-list/reviews-list.jsx";
 import {connect} from "react-redux";
@@ -17,30 +17,26 @@ const ReviewsFormWrapped = withReviewForm(ReviewsForm);
 
 const MAX_IMAGES = 6;
 const COEFFICIENT_ROUNDING_STAR = 20;
-class ApartmentDetailInfo extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const ApartmentDetailInfo = (props) => {
 
-  render() {
-    const {offersNearby = [], authorizationStatus, offer, activeOfferId} = this.props;
+  const {offersNearby = [], authorizationStatus, offer, activeOfferId} = props;
 
-    if (offer === undefined) {
-      return <PageNotFound />;
-    } else {
-      const {id, title, price, imageURLs = [], description, premium, type, rate, bedrooms, maxGuests, apartmentDetails, ownerInfo, favorite, city} = offer;
-      let offersNearbyForMap = offersNearby.slice();
-      offersNearbyForMap.push(offer);
+  if (offer === undefined) {
+    return <PageNotFound />;
+  } else {
+    const {id, title, price, imageURLs = [], description, premium, type, rate, bedrooms, maxGuests, apartmentDetails, ownerInfo, favorite, city} = offer;
+    let offersNearbyForMap = offersNearby.slice();
+    offersNearbyForMap.push(offer);
 
-      return (
-        <React.Fragment>
-          <div className="page" id={id}>
-            <PageHeader />
-            <main className="page__main page__main--property">
-              <section className="property">
-                <div className="property__gallery-container container">
-                  <div className="property__gallery">
-                    {imageURLs
+    return (
+      <React.Fragment>
+        <div className="page" id={id}>
+          <PageHeader />
+          <main className="page__main page__main--property">
+            <section className="property">
+              <div className="property__gallery-container container">
+                <div className="property__gallery">
+                  {imageURLs
                     .slice(0, MAX_IMAGES)
                     .map((item, index) => {
                       return (
@@ -49,113 +45,112 @@ class ApartmentDetailInfo extends PureComponent {
                         </div>
                       );
                     })}
-                  </div>
                 </div>
-                <div className="property__container container">
-                  <div className="property__wrapper">
-                    {premium ?
-                      <div className="property__mark">
-                        <span>Premium</span>
-                      </div>
-                      : ``
-                    }
-                    <div className="property__name-wrapper">
-                      <h1 className="property__name">
-                        {title}
-                      </h1>
-                      <BookmarkButton
-                        buttonClass={`property__bookmark`}
-                        detailPage={true}
-                        offerId={id}
-                        favorite={favorite}
-                      />
+              </div>
+              <div className="property__container container">
+                <div className="property__wrapper">
+                  {premium ?
+                    <div className="property__mark">
+                      <span>Premium</span>
                     </div>
-                    <div className="property__rating rating">
-                      <div className="property__stars rating__stars">
-                        <span style={{width: `${COEFFICIENT_ROUNDING_STAR * Math.round(rate)}%`}}></span>
-                        <span className="visually-hidden">Rating</span>
-                      </div>
-                      <span className="property__rating-value rating__value">{rate}</span>
-                    </div>
-                    <ul className="property__features">
-                      <li className="property__feature property__feature--entire">
-                        {type}
-                      </li>
-                      <li className="property__feature property__feature--bedrooms">
-                        {bedrooms} Bedrooms
-                      </li>
-                      <li className="property__feature property__feature--adults">
-                  Max {maxGuests} adults
-                      </li>
-                    </ul>
-                    <div className="property__price">
-                      <b className="property__price-value">€{price}</b>
-                      <span className="property__price-text">&nbsp;night</span>
-                    </div>
-                    <div className="property__inside">
-                      <h2 className="property__inside-title">What`s inside</h2>
-                      <ul className="property__inside-list">
-                        {apartmentDetails.map((item, index) => {
-                          return (
-                            <li className="property__inside-item" key={item + index}>
-                              {item}
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    </div>
-                    <div className="property__host">
-                      <h2 className="property__host-title">Meet the host</h2>
-                      <div className="property__host-user user">
-                        <div className={`property__avatar-wrapper ${ownerInfo.super ? ` property__avatar-wrapper--pro` : ``} user__avatar-wrapper`}>
-                          <img className="property__avatar user__avatar" src={`../${ownerInfo.srcAvatar}`} width={74} height={74} alt="Host avatar" />
-                        </div>
-                        <span className="property__user-name">
-                          {ownerInfo.name}
-                        </span>
-                      </div>
-                      <div className="property__description">
-                        <p className="property__text">
-                          {description}
-                        </p>
-                      </div>
-                    </div>
-                    <section className="property__reviews reviews">
-                      <ReviewsList
-                        offerId={id}
-                      />
-                      {authorizationStatus === AuthorizationStatus.AUTH
-                        ? <ReviewsFormWrapped offerId={id} />
-                        : null
-                      }
-                    </section>
-                  </div>
-                </div>
-                <Map
-                  offersShow={offersNearbyForMap}
-                  isDetail={true}
-                  activeCity={city}
-                  isDetailHoverId={id}
-                />
-              </section>
-              <div className="container">
-                <section className="near-places places">
-                  <h2 className="near-places__title">Other places in the neighbourhood</h2>
-                  <div className="near-places__list places__list">
-                    <OffersNearby
-                      activeOfferId={activeOfferId}
-                      history={history}
+                    : ``
+                  }
+                  <div className="property__name-wrapper">
+                    <h1 className="property__name">
+                      {title}
+                    </h1>
+                    <BookmarkButton
+                      buttonClass={`property__bookmark`}
+                      detailPage={true}
+                      offerId={id}
+                      favorite={favorite}
                     />
                   </div>
-                </section>
+                  <div className="property__rating rating">
+                    <div className="property__stars rating__stars">
+                      <span style={{width: `${COEFFICIENT_ROUNDING_STAR * Math.round(rate)}%`}}></span>
+                      <span className="visually-hidden">Rating</span>
+                    </div>
+                    <span className="property__rating-value rating__value">{rate}</span>
+                  </div>
+                  <ul className="property__features">
+                    <li className="property__feature property__feature--entire">
+                      {type}
+                    </li>
+                    <li className="property__feature property__feature--bedrooms">
+                      {bedrooms} Bedrooms
+                    </li>
+                    <li className="property__feature property__feature--adults">
+                  Max {maxGuests} adults
+                    </li>
+                  </ul>
+                  <div className="property__price">
+                    <b className="property__price-value">€{price}</b>
+                    <span className="property__price-text">&nbsp;night</span>
+                  </div>
+                  <div className="property__inside">
+                    <h2 className="property__inside-title">What`s inside</h2>
+                    <ul className="property__inside-list">
+                      {apartmentDetails.map((item, index) => {
+                        return (
+                          <li className="property__inside-item" key={item + index}>
+                            {item}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                  <div className="property__host">
+                    <h2 className="property__host-title">Meet the host</h2>
+                    <div className="property__host-user user">
+                      <div className={`property__avatar-wrapper ${ownerInfo.super ? ` property__avatar-wrapper--pro` : ``} user__avatar-wrapper`}>
+                        <img className="property__avatar user__avatar" src={`../${ownerInfo.srcAvatar}`} width={74} height={74} alt="Host avatar" />
+                      </div>
+                      <span className="property__user-name">
+                        {ownerInfo.name}
+                      </span>
+                    </div>
+                    <div className="property__description">
+                      <p className="property__text">
+                        {description}
+                      </p>
+                    </div>
+                  </div>
+                  <section className="property__reviews reviews">
+                    <ReviewsList
+                      offerId={id}
+                    />
+                    {authorizationStatus === AuthorizationStatus.AUTH
+                      ? <ReviewsFormWrapped offerId={id} />
+                      : null
+                    }
+                  </section>
+                </div>
               </div>
-            </main>
-          </div>
-        </React.Fragment>
-      );
-    }
+              <Map
+                offersShow={offersNearbyForMap}
+                isDetail={true}
+                activeCity={city}
+                isDetailHoverId={id}
+              />
+            </section>
+            <div className="container">
+              <section className="near-places places">
+                <h2 className="near-places__title">Other places in the neighbourhood</h2>
+                <div className="near-places__list places__list">
+                  <OffersNearby
+                    activeOfferId={activeOfferId}
+                    history={history}
+                  />
+                </div>
+              </section>
+            </div>
+          </main>
+        </div>
+      </React.Fragment>
+    );
   }
-}
+};
 
 ApartmentDetailInfo.propTypes = {
   offer: PropTypes.shape({
